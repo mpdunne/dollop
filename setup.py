a4acebf6e44dd3751bdefd9680cb86cf90fa00ae
Michael Dunne <mpdunne@live.com>
2023-12-28 15:00:47 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt", "r") as file_handler:
+with open('requirements.txt', 'r') as file_handler:
     requirements = file_handler.readlines()
 
 setup(
-    name="dollop",
-    version="0.0.0",
+    name='dollop',
+    version='1.0.0',
+    author='Michael Peter Dunne',
     include_package_data=True,
     install_requires=requirements,
-    packages=find_packages(include=["dollop", "dollop.*"]),
+    packages=find_packages(include=['dollop', 'dollop.*']),
     classifiers=[
-        "Programming Language :: Python :: 3.0",
-        "Intended Audience :: Everyone",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
     ],
 )
